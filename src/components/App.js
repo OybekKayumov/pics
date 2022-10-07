40e827b768e1958d72faf231037bfcc51c1c3094
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-07 17:30:32 +0500

    Showing Forms

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <SearchBar>
+        
+      </SearchBar>  
+    </div>
   )
 };
 
