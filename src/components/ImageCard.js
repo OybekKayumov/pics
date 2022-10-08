1899ff077e1b2603abb5e4cdb9a68249e6a42353
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-08 13:53:28 +0500

    Add constructor in ImageCard

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.imageRef = React.createRef();
+  }
+  
+  
+
   render() {
+    const { description, urls } = this.props.image;
     return (
       <div>
         <img 
-          src={this.props.image.urls.regular} 
-          alt={this.props.image.description} />
+          // src={this.props.image.urls.regular} 
+          // alt={this.props.image.description} 
+          
+          alt={description}
+          src={urls.regular}
+          ref={this.imageRef}
+        />
       </div>
     )
   }
