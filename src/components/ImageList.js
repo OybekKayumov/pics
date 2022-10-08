760d2cc215f327ce02d0ac2d5fe686e48bda7b25
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-08 11:29:06 +0500

    Show pics - destruction

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const ImageList = (props) => {
   // console.log('images:', props.images);
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} alt="regular img" />    
+  // const images = props.images.map((image ) => {
+  //   return <img key={image.id} src={image.urls.regular} alt={image.description} />    
+  // })
+
+  // destructuring
+  const images = props.images.map(({ id, urls, description }) => {
+    return <img key={id} src={urls.regular} alt={description} />    
   })
 
   return (
