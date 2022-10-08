84c0ef9044251be8cb7c44cc85d9418d551aa6ad
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-08 13:40:31 +0500

    Add import component ImageCard into ImageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,17 @@
 import React from "react";
+import ImageCard from "./ImageCard";
 import './ImageList.css'
 
 const ImageList = (props) => {
   // console.log('images:', props.images);
-  // const images = props.images.map((image ) => {
-  //   return <img key={image.id} src={image.urls.regular} alt={image.description} />    
-  // })
+  const images = props.images.map((image ) => {
+    return <ImageCard key={image.id} image={image} />    
+  })
 
   // destructuring
-  const images = props.images.map(({ id, urls, description }) => {
-    return <img key={id} src={urls.regular} alt={description} />    
-  })
+  // const images = props.images.map(({ id, urls, description }) => {
+  //   return <img key={id} src={urls.regular} alt={description} />    
+  // })
 
   return (
     <div className="image-list">
