27bb81fb686d6d497475f5201baeb9f5c7d92fda
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-08 08:33:29 +0500

    Add api folder and create unsplach axios to fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,28 @@
 import React from "react";
 import axios from "axios";
+import unspalsh from "../api/unspalsh";
 import SearchBar from "./SearchBar";
 import Validator from "../exercises/Validator";
 
-// const App = () => {
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit= async (term) => {
     console.log('App term: ', term);
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID Access_Key'
-      }
-    })
-    // .then((response) => {
-    //   console.log('response: ', response);
-    //   console.log('response: ', response.data.results);
-    // })
+    const response = await unspalsh.get('/search/photos', {
+      params: { query: term },    
+    })    
 
     console.log(response.data.results);
+    console.log('this:', this);
+    this.setState({ images: response.data.results });
   } 
 
   render () {  
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}>
-        </SearchBar>
+        <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
+        Found: {this.state.images.length} images
       </div>
     )
   }
@@ -34,3 +31,8 @@ class App extends React.Component {
 export default App;
 
 {/* <Validator></Validator> */}
+
+// .then((response) => {
+    //   console.log('response: ', response);
+    //   console.log('response: ', response.data.results);
+    // })
